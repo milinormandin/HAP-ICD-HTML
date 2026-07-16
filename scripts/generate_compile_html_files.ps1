abbddae608ac82fc02a337e1e2eaac5eb5ad1f3c
mili <mnormand@hawaii.edu>
2026-07-16 13:20:21 -1000

    Added global CSS file

--- scripts/generate_compile_html_files.ps1
+++ scripts/generate_compile_html_files.ps1
@@ -11,7 +11,7 @@ Set-Location $currentPath
 $outputPath = ""
 $output = ""
 $icdfolder = ""
-$indexoutput = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Test</title></head><body><ul>'
+$indexoutput = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Test</title><link rel="stylesheet" href="index.css"></head><body><ul>'
 
 
 
